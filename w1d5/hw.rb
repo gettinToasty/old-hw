25e0ee24cc974e6b5e59cb507be68dd0978631c2
gettinToasty <sean.r.beyer@gmail.com>
2016-11-17 21:23:11 -0800

    Implement Queue class

--- w1d5/hw.rb
+++ w1d5/hw.rb
@@ -17,3 +17,23 @@ class Stack
   end
 
 end
+
+class Queue
+
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue << el
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def show
+    @queue
+  end
+
+end
