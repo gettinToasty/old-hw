7b3da5e264392cab38f79eb970f4b326875b9b47
gettinToasty <sean.r.beyer@gmail.com>
2016-11-17 21:44:28 -0800

    Implement Map class

--- w1d5/hw.rb
+++ w1d5/hw.rb
@@ -37,3 +37,24 @@ class Queue
   end
 
 end
+
+class Map
+
+  def initialize
+    @map = []
+  end
+
+  def assign(key, val)
+    @map.each_index { |i| return @map[i][1] = val if @map[i][0] == key }
+    @map << [key, val]
+  end
+
+  def lookup(key)
+    @map.each_index { |i| return @map[i][1] if @map[i][0] == key }
+  end
+
+  def remove(key)
+    @map.each_index { |i| return @map.delete_at(i) if @map [i][0] == key }
+  end
+
+end
