1c6688dc7cf592c2bc08ff0c8708032e59c18168
gettinToasty <sean.r.beyer@gmail.com>
2016-12-03 16:19:56 -0800

    Begin Controllers HW

---
+++ w4d1/library/app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    render :index
+  end
+
+  def new
+    # your code here
+  end
+
+  def create
+    # your code here
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to 'books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+end
