84237e86837348acbe3a79b9a76c22afaf51cf0b
gettinToasty <sean.r.beyer@gmail.com>
2016-12-22 17:52:32 -0800

    Finish NPM setup hw

---
+++ w6d5/skeleton/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: 'entry.jsx',
+  output: {
+    path: './',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx' ]
+  }
+};
